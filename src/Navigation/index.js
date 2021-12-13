afc97ce83a23834ec038110c86aaf689bea165d4
code-diggers-369 <codediggers007@gmail.com>
2021-12-13 12:51:36 +0530

    set navigation and splashscreen"

---
+++ src/Navigation/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+// import screen views
+import SplashScreen from '../View/SplashScreen';
+
+const Stack = createNativeStackNavigator();
+
+function Navigation() {
+  return (
+    <NavigationContainer initialState="Splash">
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Splash"
+          component={SplashScreen}
+          options={{headerShown: false}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default Navigation;
---
+++ src/View/SplashScreen.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import {View, Text, StatusBar, StyleSheet, Image} from 'react-native';
+
+// import logo
+import LogoImg from '../../assets/logo.png';
+
+export default function SplashScreen() {
+  return (
+    <View style={customStyle.container}>
+      <StatusBar backgroundColor={'transparent'} translucent={true} />
+      <Text style={customStyle.titleText}>Pikashow</Text>
+
+      <Image style={customStyle.logo} source={LogoImg} />
+
+      <Text style={customStyle.bottomText}>Made With ♥ In India</Text>
+    </View>
+  );
+}
+
+const customStyle = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#111111',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  titleText: {
+    color: '#fff',
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  logo: {
+    height: 200,
+    width: 200,
+  },
+  bottomText: {
+    color: '#fff',
+  },
+});

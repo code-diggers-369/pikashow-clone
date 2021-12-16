be3350ba6f6e0d9e6b82a6c388f9efb5bd69fb82
code-diggers-369 <codediggers007@gmail.com>
2021-12-16 13:14:26 +0530

    auto navigation splash to home screen

--- src/Navigation/index.js
+++ src/Navigation/index.js
@@ -1,20 +1,38 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useEffect, useState} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 // import screen views
 import SplashScreen from '../View/SplashScreen';
+import HomeScreen from '../View/HomeScreen';
 
 const Stack = createNativeStackNavigator();
 
 function Navigation() {
+  const [isSplashScreenVisible, setIsSplashScreenVisible] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsSplashScreenVisible(false);
+    }, 3000);
+    return () => {};
+  }, []);
+
   return (
-    <NavigationContainer initialState="Splash">
-      <Stack.Navigator>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Splash">
+        {/* splash screen */}
+        {isSplashScreenVisible ? (
+          <Stack.Screen
+            name="Splash"
+            component={SplashScreen}
+            options={{headerShown: false}}
+          />
+        ) : null}
+        {/* home screen */}
         <Stack.Screen
-          name="Splash"
-          component={SplashScreen}
+          name="Home"
+          component={HomeScreen}
           options={{headerShown: false}}
         />
       </Stack.Navigator>

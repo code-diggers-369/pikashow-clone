75f6d2cc87feba99457c6a6ad8d65238fe314e4c
code-diggers-369 <codediggers007@gmail.com>
2022-01-14 14:24:57 +0530

    add movie card & data in bollywood screen

--- src/View/BollywoodScreen.js
+++ src/View/BollywoodScreen.js
@@ -1,10 +1,41 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet, FlatList} from 'react-native';
+
+// import card
+import Card from '../Components/MovieListCard';
+
+// import data
+import MovieList from '../Utils/BollywoodMovieList';
 
 export default function BollywoodScreen() {
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Bollywood</Text>
+    <View style={styles.container}>
+      <Text style={styles.headerText}>Bollywood</Text>
+
+      <View style={{marginBottom: 50}}>
+        <FlatList
+          data={MovieList}
+          renderItem={list => {
+            return <Card movieData={list.item} />;
+          }}
+          numColumns={3}
+        />
+        {/* {MovieList.map((movieData, index) => {
+          return <Card />;
+        })} */}
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000000',
+  },
+  headerText: {
+    color: '#ffe031',
+    textAlign: 'center',
+    marginTop: 20,
+  },
+});
